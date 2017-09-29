5d70c24ada3d9baed11ef4b03f01723004a29f2e
Andrey K <andrey.k@smartum.pro>
2017-09-29 12:21:43 +0300

    Correct initial migration. Add prod.gradle

--- sql/migration/V20170928163656__initial_version.sql
+++ sql/migration/V20170928163656__initial_version.sql
@@ -5,16 +5,22 @@ CREATE TYPE PUSH_DEVICE_TYPE AS ENUM ('ANDROID', 'IOS');
 
 CREATE TABLE conversation
 (
-    id                BIGSERIAL PRIMARY KEY,
-    type              VARCHAR(30) NOT NULL,
-    slack_user_id     VARCHAR(100) DEFAULT NULL,
-    slack_channel_id  VARCHAR(100) DEFAULT NULL,
-    fb_sender_id      VARCHAR(100) DEFAULT NULL,
-    fb_recipient_id   VARCHAR(100) DEFAULT NULL,
-    tg_chat_id        VARCHAR(100) DEFAULT NULL,
-    tg_sender_name    VARCHAR(100) DEFAULT NULL,
-    "timestamp"       TIMESTAMP
-)
+    id                          BIGSERIAL PRIMARY KEY,
+    type                        VARCHAR(30) NOT NULL,
+    sender_name                 VARCHAR(100) DEFAULT NULL,
+    photo_url                   VARCHAR(500) DEFAULT NULL,
+    "timestamp"                 TIMESTAMP,
+    slack_user_id               VARCHAR(100) DEFAULT NULL,
+    slack_channel_id            VARCHAR(100) DEFAULT NULL,
+    fb_sender_id                VARCHAR(100) DEFAULT NULL,
+    fb_recipient_id             VARCHAR(100) DEFAULT NULL,
+    tg_chat_id                  VARCHAR(100) DEFAULT NULL,
+    skype_sender_id             VARCHAR(100) DEFAULT NULL,
+    skype_conversation_id       VARCHAR(100) DEFAULT NULL,
+    skype_access_token          VARCHAR(500) DEFAULT NULL,
+    skype_access_token_exp_date TIMESTAMP DEFAULT NULL
+
+);
 
 CREATE TABLE message
 (
@@ -22,14 +28,14 @@ CREATE TABLE message
     text            VARCHAR(10000)  NOT NULL,
     "timestamp"     TIMESTAMP       NOT NULL,
     conversation_id BIGINT          REFERENCES conversation (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    botreply        BOOLEAN         DEFAULT false,
-)
+    botreply        BOOLEAN         DEFAULT false
+);
 
 CREATE TABLE push_device
 (
     id              BIGSERIAL PRIMARY KEY,
     token           VARCHAR(1000)     NOT NULL,
     device_type     PUSH_DEVICE_TYPE  NOT NULL
-)
+);
 
 COMMIT;
